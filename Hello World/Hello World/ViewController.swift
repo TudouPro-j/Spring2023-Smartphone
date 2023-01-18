2eae51fc603e2b1f71679753691de35a0cf36729
Shuyao Wang <shuyaowanghdj@gmail.com>
2023-01-17 23:11:22 -0800

    Checking in code for Hello world Project

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by shuyao on 1/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lblExample: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("I am inside viewDidLiad() function")
+    }
+
+    @IBAction func pressMeAction(_ sender: UIButton) {
+        print("Button was pressed")
+        lblExample.text = "Button was pressed"
+    }
+    
+}
+
